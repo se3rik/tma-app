435e3f3f878e39cc68fac284e457cade6197b31d
se3rik <se3rik@mail.ru>
2024-11-07 19:30:52 +0300

    feat: twa-sdk integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+import WebApp from "@twa-dev/sdk";
 import "./assets/tailwind.css";
 import App from "./App.vue";
+
+WebApp.ready();
 
 const app = createApp(App);
 
